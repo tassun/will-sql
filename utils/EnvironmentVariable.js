a668d971a4a20ae28e64edc808541f26986d01f8
tassun <tassun_oro@hotmail.com>
2022-10-15 12:09:45 +0700

    remove js file

--- utils/EnvironmentVariable.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_PASSWORD = exports.DB_USER = exports.DB_DIALECT = exports.DB_ALIAS = exports.DB_URL = void 0;
-exports.DB_URL = process.env.DB_URL || "mysql://root:root@localhost:3306/basedb?charset=utf8&connectionLimit=10";
-exports.DB_ALIAS = process.env.DB_ALIAS || "mysql";
-exports.DB_DIALECT = process.env.DB_DIALECT || "mysql";
-exports.DB_USER = process.env.DB_USER || "";
-exports.DB_PASSWORD = process.env.DB_PASSWORD || "";
